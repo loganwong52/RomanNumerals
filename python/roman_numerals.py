f1055ed76696bfb46c96cef5a27330ea2f232706
loganwong52 <loganwong52@gmail.com>
2022-05-24 14:23:28 -0700

    Logan Wong Python Roman Numeral Solution

--- python/roman_numerals.py
+++ python/roman_numerals.py
@@ -1,2 +1,37 @@
 def to_roman(num):
     # write your code here!
+    output = ""
+    roman_numeral_map = {
+        "M" : 1000,
+        "CM" : 900,
+        "D" : 500,
+        "CD" : 400,
+        "C" : 100,
+        "XC" : 90,
+        "L" : 50,
+        "XL" : 40,
+        "X" : 10,
+        "IX" : 9,
+        "V" : 5,
+        "IV" : 4,
+        "I" : 1
+    }
+
+
+    while(num > 0):
+        # 4. Iterate over ROMAN_NUMERAL_TO_ARABIC_MAP, keep track of ROMAN_NUMERAL and ARABIC_NUMBER
+        for roman_num in roman_numeral_map:
+            arabic_num = roman_numeral_map[roman_num]
+            
+            # 5. Set EVENLY_DIVISIBLE_TIMES = INPUT_NUMBER / ARABIC_NUMBER:
+            evenly_div_times = num//arabic_num
+        
+            # 6. If EVENLY_DIVISIBLE_TIMES >= 1
+            if(evenly_div_times >= 1):
+                for i in range(0, evenly_div_times):
+                    # 6a. Append ROMAN_NUMERAL to OUTPUT EVENLY_DIVISIBLE_TIMES
+                    output += roman_num
+                    # 6b. Subtract ARABIC_NUMBER from INPUT_NUMBER EVENLY_DIVISIBLE_TIMES
+                    num -= arabic_num
+
+    return output
--- python/roman_numerals_spec.py
+++ python/roman_numerals_spec.py
@@ -4,3 +4,10 @@ print(to_roman(1) == 'I')
 print(to_roman(3) == 'III')
 print(to_roman(4) == 'IV')
 # add tests to cover different edge cases
+
+print(to_roman(49));     #XLIX
+print(to_roman(99));     #XCIX
+print(to_roman(499));    #CDXCIX
+print(to_roman(944));    #CMXLIV
+print(to_roman(999));    #CMXCIX
+print(to_roman(150));    #CL
